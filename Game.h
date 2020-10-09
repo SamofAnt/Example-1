d9ca734b3db52958ed9cd56fe703fb91b8fa6157
TheEvilShot <72579711+TheEvilShot@users.noreply.github.com>
2020-10-09 20:00:28 +0300

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+unsigned int Throw();
+bool WhoMovesFirst();
+void PrintCube(unsigned int );
+void PrintScore(unsigned int, unsigned int)
